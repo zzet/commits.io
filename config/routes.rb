b8c9070cd2c6f118f7f247064f75101714b03d08
Andrey Kumanyaev <me@zzet.org>
2013-03-16 20:00:31 +0400

    Add hook controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ CommitsIO::Application.routes.draw do
       get :authorization, :on => :member
     end
 
+    resource :hook, only: :create
+
     # private
     get '/dashboard' => 'users#dashboard'
     get '/profile' => 'users#profile'
