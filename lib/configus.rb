8026ac5615e5195474ef4000aceb42825902c14e
Andrey Kumanyaev <me@zzet.org>
2013-03-16 23:48:40 +0400

    update configus settings

--- lib/configus.rb
+++ lib/configus.rb
@@ -1,9 +1,17 @@
 Configus.build Rails.env do
   env :production do
-
+    system do
+      repositories do
+        path Rails.root.join('data', 'repositories')
+      end
+    end
   end
 
   env :development, parent: :production do
-    system.repositories.path Rails.root.join('data', 'repositories')
+    system do
+      repositories do
+        path Rails.root.join('data', 'repositories')
+      end
+    end
   end
 end
