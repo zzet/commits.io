56c8205c1334a2672daa9a26f5e9b989a3d026c2
Andrew8xx8 <avk@8xx8.ru>
2013-03-17 00:47:46 +0400

    Profanity metric added

---
+++ lib/metrics/commit_message_profanity.rb
@@ -0,0 +1,24 @@
+class Metrics::CommitMessageProfanity < Metrics::Metric
+  def calculate(metrics)
+    profanity = check @data[:message]
+
+    metrics << {
+      metric: :commit_message_profanity,
+      data: {
+        words: profanity[:all],
+        wrong_words: profanity[:wrong],
+        rate: 1 - (profanity[:wrong].count.to_f / profanity[:all].count)
+      }
+    }
+  end
+
+  private
+
+  def check(text)
+    {
+      all: split_words(text),
+      wrong: Obscenity.offensive(text)
+    }
+  end
+
+end
---
+++ test/unit/lib/metrics/commit_message_profanity_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Metrics::CommitMessageProfanityTest < ActiveSupport::TestCase
+  def test_could_calculate_metric
+    data = generate :metrics_message
+    data[:message] = "Test fucking message wohooo!"
+
+    metric = Metrics::CommitMessageProfanity.new data
+    metrics = metric.calculate([])
+    assert_equal metrics, [{
+      :metric => :commit_message_profanity,
+      :data => {
+        :words => ["Test", "fucking", "message", "wohooo"],
+        :wrong_words=>["fucking"],
+        :rate=>0.75
+      }
+    }]
+  end
+end
