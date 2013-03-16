0b25ba616b5ca2659cc9c205ed20b57f7dd6bca7
Andrey Kumanyaev <me@zzet.org>
2013-03-16 19:33:23 +0400

    method for hook

--- lib/cio/sync/repository.rb
+++ lib/cio/sync/repository.rb
@@ -4,5 +4,9 @@ class Cio::Sync::Repository
       cmd = "cd #{configus.system.repositories.path} && git clone --bare #{repository.source} #{repository.owner}/#{repository.name}"
       system(cmd)
     end
+
+    def update(reposutory, push_data)
+      # UPDATE repository via hook push data
+    end
   end
 end
