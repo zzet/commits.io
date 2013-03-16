1977aa1d8fb86a72f739e6810ade72d04d65fd1f
Anton Kalyaev <anton.kalyaev@gmail.com>
2013-03-16 12:10:49 +0100

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316103509) do
+ActiveRecord::Schema.define(:version => 20130316105840) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -45,19 +45,19 @@ ActiveRecord::Schema.define(:version => 20130316103509) do
     t.datetime "updated_at",  :null => false
   end
 
+  create_table "organizations", :force => true do |t|
+    t.string   "name"
+    t.string   "company"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "user_auth_tokens", :force => true do |t|
     t.integer  "user_id"
     t.string   "authentication_token"
     t.datetime "expired_at"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
-  end
-
-  create_table "user_badges", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "badge_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
