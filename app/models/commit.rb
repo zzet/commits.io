7511854757ee459b91c4fb9e5a18c4a249de84c0
Anton Kalyaev <anton.kalyaev@gmail.com>
2013-03-17 02:08:53 +0100

    made parent_sha not required

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -9,7 +9,7 @@ class Commit < ActiveRecord::Base
   validates :committer, presence: true
   validates :repository, presence: true
   validates :sha, presence: true, length: { maximum: 255 }
-  validates :parent_sha, presence: true, length: { maximum: 255 }
+  validates :parent_sha, length: { maximum: 255 }
   validates :message, presence: true, length: { maximum: 255 }
 
   scope :top, -> { joins(:commit_metrics).select('commits.id, SUM(commit_metrics.percent) as total_percent').group('commits.id').order('total_percent ASC') }
