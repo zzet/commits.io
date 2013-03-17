2002c17f6c9676bc419290af3e1179ebd785cd14
Anton Kalyaev <anton.kalyaev@gmail.com>
2013-03-17 05:06:35 +0100

    fixed routes order

--- app/controllers/web/repositories/commits_controller.rb
+++ app/controllers/web/repositories/commits_controller.rb
@@ -1,10 +1,11 @@
 class Web::Repositories::CommitsController < Web::Repositories::ApplicationController
 
   def index
-    @repository = Repository.find_by_name!(params[:repository])
+    @repository = resource_repository
     @commits = @repository.commits
   end
 
   def show
+    @commit = Commit.find_by_sha(params[:sha])
   end
 end
