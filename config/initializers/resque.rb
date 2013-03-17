d4209ab6a94de69ffaaa4a9d6f178ad037be02ab
Andrey Kumanyaev <me@zzet.org>
2013-03-17 05:13:49 +0400

    New redis path

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,4 @@
-uri = URI.parse("redis://localhost:6379/")
+uri = URI.parse("redis://gitlab.platform.home:6379/")
+#uri = URI.parse("redis://localhost:6379/")
 #uri = URI.parse("redis://192.168.3.14:6379/")
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
