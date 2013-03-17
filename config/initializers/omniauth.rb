a2b526538bedd16e1dbb08c0f3abd9dd9b665933
Andrew8xx8 <avk@8xx8.ru>
2013-03-17 07:14:00 +0400

    Production config added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, "cf2866e191ced6227026", "9c56321851a5d6aaea569b88286b04e08eb6dc75"
+  if Rails.env.to_sym == :production
+    provider :github, "4381010f282e6b04c5d4", "679ad981db19e01581bd3bd632176eb8560c2a07"
+  else
+    provider :github, "cf2866e191ced6227026", "9c56321851a5d6aaea569b88286b04e08eb6dc75"
+  end
 end
