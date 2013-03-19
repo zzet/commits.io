6246e46b692bf4d9f1706db31869072923ded062
Andrew8xx8 <avk@8xx8.ru>
2013-03-19 21:16:27 +0400

    Server address updated

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 set :user, 'commits_io_production'
 set :rails_env, 'production'
 
-role :db,  "62.76.186.131", primary: true
-role :app, "62.76.186.131"
-role :web, "62.76.186.131"
+role :db,  "commits.io", primary: true
+role :app, "commits.io"
+role :web, "commits.io"
 
 set :branch, "master"
 set :deploy_to, "/u/apps/commits.io"
